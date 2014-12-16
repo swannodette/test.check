d3dd6bbd0c113fd486133814010fcc658277fb8b
dnolen <dnolen@cognitect.com>
2014-12-16 15:51:35 -0500

    building

--- project.clj
+++ project.clj
@@ -6,7 +6,23 @@
   :dependencies []
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/clojurescript "0.0-2488"]]}}
   :global-vars {*warn-on-reflection* true}
-  :plugins [[codox "0.8.10"]]
-  :codox {:defaults {:doc/format :markdown}})
+  :plugins [[codox "0.8.10"]
+            [lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :codox {:defaults {:doc/format :markdown}}
+  :cljsbuild
+  {:builds
+   [{:id "dev"
+     :source-paths ["src/main/clojure/cljs" "src/test/clojure/cljs"]
+     :compiler {:optimizations :none
+                :output-to "target/cljs/test-dev/tests.js"
+                :output-dir "target/cljs/test-dev/out"
+                :source-map true}}
+    {:id "adv"
+     :source-paths ["src/main/clojure/cljs" "src/test/clojure/cljs"]
+     :compiler {:optimizations :advanced
+                :pretty-print false
+                :output-to "target/cljs/test-adv/tests.js"
+                :output-dir "target/cljs/test-adv/out"}}]})
---
+++ src/test/clojure/cljs/test/check/test.cljs
@@ -0,0 +1,19 @@
+(ns cljs.test.check.test
+  (:require [cljs.test :as test :refer-macros [deftest testing is]]
+            [cljs.test.check.generators :as gen]
+            [cljs.test.check.properties :as prop :include-macros true]
+            [cljs.test.check.rose-tree :as rose]
+            [cljs.test.check.cljs-test :as ct :refer-macros [defspec]]
+            [cljs.reader :as r]))
+
+(deftest generators-are-generators
+  (testing "generator? returns true when called with a generator"
+    (is (gen/generator? gen/int))
+    (is (gen/generator? (gen/vector gen/int)))
+    (is (gen/generator? (gen/return 5)))))
+
+(deftest values-are-not-generators
+  (testing "generator? returns false when called with a value"
+    (is (not (gen/generator? 5)))
+    (is (not (gen/generator? int)))
+    (is (not (gen/generator? [1 2 3])))))
